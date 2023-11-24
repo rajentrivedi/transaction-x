358ec0ceee8d3eb951cee86876b04f853877bd5d
Rajen Trivedi <rajen.trivedi@indianic.com>
2023-11-24 14:39:11 +0530

    namespace issue resolved

--- src/Facades/TransactionX.php
+++ src/Facades/TransactionX.php
@@ -4,9 +4,7 @@ namespace RajenTrivedi\TransactionX\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @see \Rajen Trivedi\TransactionX\TransactionX
- */
+
 class TransactionX extends Facade
 {
     protected static function getFacadeAccessor()
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Rajen Trivedi\TransactionX\Tests;
+namespace RajenTrivedi\TransactionX\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Rajen Trivedi\TransactionX\TransactionXServiceProvider;
+use RajenTrivedi\TransactionX\TransactionXServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Rajen Trivedi\\TransactionX\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'RajenTrivedi\\TransactionX\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
